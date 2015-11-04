c5126608f6ea53ad1d3c3d5653b3f4d3ca4e2c50
Dmitry Osipov <ibakepunk@gmail.com>
2015-11-05 00:52:58 +0600

    Implement iptables configurer

--- wookie.py
+++ wookie.py
@@ -1,4 +1,8 @@
-import sh
+from subprocess import check_call, CalledProcessError
+
+import sys
+
+from args_parser import parser
 
 class FirewallConfigurer:
     def __init__(self, firewall_binary):
@@ -10,18 +14,50 @@ class FirewallConfigurer:
     def close_ports(self, ports):
         pass
 
+    def clear_rules(self):
+        pass
+
 
 class IptablesConfigurer(FirewallConfigurer):
     MAX_PORTS = 15
+
     # iptables -A INPUT -p tcp -m multiport --dports "22,443,80,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16" -j ACCEPT
     def open_ports(self, ports):
-        print 'ports input: ', str(ports)
         for chunk in chunks(ports, self.MAX_PORTS):
-            print 'chunk: ', str(chunk)
-            sh.iptables(A='INPUT', p='tcp', m='multiport', dports='"'+str(chunk)[1:-1]+'"', j='ACCEPT')
+            try:
+                check_call([self.firewall_binary,
+                      '-A', 'INPUT',
+                      '-p', 'tcp',
+                      '-m', 'multiport',
+                      '--dports', str(chunk)[1:-1],
+                      '-j', 'ACCEPT'])
+            except CalledProcessError as e:
+                print e
+                exit(e.returncode)
+
+    def close_ports(self, ports):
+        for chunk in chunks(ports, self.MAX_PORTS):
+            try:
+                check_call([self.firewall_binary,
+                      '-A', 'INPUT',
+                      '-p', 'tcp',
+                      '-m', 'multiport',
+                      '--dports', str(chunk)[1:-1],
+                      '-j', 'DROP'])
+            except CalledProcessError as e:
+                print e
+                exit(e.returncode)
+
+    def clear_rules(self):
+        try:
+            check_call([self.firewall_binary, '--flush'])
+        except CalledProcessError as e:
+            print e
+            exit(e.returncode)
 
 class FirewalldConfigurer(FirewallConfigurer):
     pass
+
 
 def chunks(iterable, chunk_size):
     for i in xrange(0, len(iterable), chunk_size):
